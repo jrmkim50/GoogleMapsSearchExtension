c158defcf92d45f6036ad78a30673317f020f2cf
jrmkim50 <jrmkim50@gmail.com>
2020-08-04 04:49:23 -0400

    got background working

--- background.js
+++ background.js
@@ -1,16 +1,95 @@
-function searchOnGoogleMaps(info, tab) {
-    var address = info.selectionText;
-    address = address.replace(/\s+/g, "+");
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {greeting: "create_popup"}, function(response) {
-          console.log(response.farewell);
-        });
-    });
-}
-
+//Create context menu button
 var title = "Search on Google Maps";
 var contexts = ["selection"];
 var id = "search_ext";
 
 chrome.contextMenus.create({ "id": id, "title": title, "contexts": contexts });
 chrome.contextMenus.onClicked.addListener(searchOnGoogleMaps);
+
+function searchOnGoogleMaps(info, tab) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { greeting: "create_popup" }, function (response) {
+      if (response) {
+        console.log(response.farewell);
+      }
+    });
+  });
+}
+
+//Load google maps script
+const script = document.createElement("script");
+script.id = "google-maps-api";
+script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCrlbAYmXbhOOFvHn4DFAFvzK48QY_R3Pk&libraries=places";
+script.async = true;
+document.body.appendChild(script);
+
+//Listen for message from popup
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.greeting == "sending_map_div") {
+    var map = createGoogleMap(createPopupMap(0,0));
+    handleGeoCoding(map, "Google", retrieveMap); //request.query
+    sendResponse({ farewell: "goodbye" });
+    return true;
+  }
+})
+
+function retrieveMap(map) {
+  // console.log(map.getDiv());
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { greeting: "resending_map", map: "test" }, function (response) {
+      console.log("hit")
+      if (response) {
+        console.log(response.farewell);
+      }
+    });
+  });
+}
+
+function createGoogleMap(googleMapDiv) {
+  if (google) {
+    var gmap = new google.maps.Map(googleMapDiv, {
+      zoom: 15,
+      center: {
+        lat: 43.642567,
+        lng: -79.387054,
+      },
+      disableDefaultUI: true,
+    })
+    return gmap;
+  }
+  return null
+}
+
+function handleGeoCoding(map, address, cb) {
+  if (map) {
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({ address: address }, function (results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        console.log(results)
+        map.setCenter(results[0].geometry.location);
+        new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+        });
+        cb(map);
+      }
+    })
+  }
+}
+
+function createPopupMap(x, y, hidden = false) {
+  var prevDiv = document.querySelector("#chromeGoogleMapsSearchPopupDiv");
+  var newPopup = document.createElement("div");
+  newPopup.id = "chromeGoogleMapsSearchPopupDiv";
+  if (prevDiv) {
+      document.body.replaceChild(newPopup, prevDiv);
+  } else {
+      document.body.appendChild(newPopup);
+  }
+  newPopup.style.left = x + 'px';
+  newPopup.style.top = y + 'px';
+  if (hidden) {
+      newPopup.style.display = "none"
+  }
+  return newPopup;
+}
--- popup.js
+++ popup.js
@@ -1,63 +1,30 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    if (request.greeting == "create_popup" && ready) {
+    if (request.greeting == "create_popup") {
         var selection = window.getSelection();
         var textRange = selection.getRangeAt(0);
         var rect = textRange.getBoundingClientRect();
-        var div = createPopupMap(rect.left, rect.top + window.pageYOffset)
-        var map_geocoder = createGoogleMap(div);
-        var map = map_geocoder[0];
-        var geocoder = map_geocoder[1];
-        handleGeoCoding(geocoder, map, selection.toString());
-        sendResponse({ farewell: rect.left });
+        var selectedText = selection.toString();
+        var div = createPopupMap(rect.left, rect.top + window.pageYOffset);
+        chrome.runtime.sendMessage({ greeting: "sending_map_div", query: selectedText, map_div: div }, function (response) {
+            if (response) {
+              console.log(response.farewell);
+            }
+        })
+        sendResponse({farewell: "goodbye"});
+        console.log("hit")
+    }
+    if (request.greeting == "resending_map") {
+        console.log(request.map);
+        sendResponse({farewell: "thank you"})
     }
 });
 
-function handleGeoCoding(geocoder, map, address) {
-    // if (geocoder && map) {
-    //     geocoder.geocode({ address: selection }, (results, status) => {
-    //         if (status === "OK") {
-    //             map.setCenter(results[0].geometry.location);
-    //             new google.maps.Marker({
-    //                 map: map,
-    //                 position: results[0].geometry.location
-    //             });
-    //         } 
-    //     })
-    // } 
-    var map = document.querySelector("#chromeGoogleMapsSearchPopupDiv");
-    var iframe = document.createElement("iframe");
-    iframe.src = "https://www.google.com/maps/embed/v1/place?key=AIzaSyAUG1jgsBghqRkt8py6Cx_HW5VusvNkm2Q&q="+address;
-    iframe.height = "100%";
-    iframe.width = "100%";
-    map.appendChild(iframe);
-}
 document.body.addEventListener("mousemove", function (event) {
     var selection = window.getSelection().toString();
     if (selection) {
-        
+
     }
 })
-
-// const script = document.createElement("script");
-// script.id = "google-maps-api";
-// script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCrlbAYmXbhOOFvHn4DFAFvzK48QY_R3Pk&libraries=places";
-// script.async = true;
-// document.body.appendChild(script);
-// var xhr = new XMLHttpRequest();
-// xhr.open("GET", "https://maps.googleapis.com/maps/api/js?key=AIzaSyCrlbAYmXbhOOFvHn4DFAFvzK48QY_R3Pk&libraries=places", true);
-// xhr.onreadystatechange = function() {
-//   if (xhr.readyState == 4) {
-//     // JSON.parse does not evaluate the attacker's scripts.
-//     console.log(xhr.responseText);
-//   }
-// }
-// xhr.send();
-
-var ready = true;
-
-// script.addEventListener("load", () => {
-//     ready = true;
-// })
 
 function createPopupMap(x, y, hidden = false) {
     var prevDiv = document.querySelector("#chromeGoogleMapsSearchPopupDiv");
@@ -74,24 +41,4 @@ function createPopupMap(x, y, hidden = false) {
         newPopup.style.display = "none"
     }
     return newPopup;
-}
-
-function createGoogleMap(googleMapDiv) {
-    console.log("hit");
-    if (window.google) {
-        console.log("good");
-        var gmap = new window.google.maps.Map(googleMapDiv, {
-            zoom: 15,
-            center: {
-                lat: 43.642567,
-                lng: -79.387054,
-            },
-            disableDefaultUI: true,
-        })
-        ggeocoder = new window.google.maps.Geocoder();
-    
-        return [gmap, ggeocoder];
-    }
-    return [null,null]
-    
 }
